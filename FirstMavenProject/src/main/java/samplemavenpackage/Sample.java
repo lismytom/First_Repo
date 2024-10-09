4d4bfb1dd49c86c47c662be83d18a7eaea72c814
lismy <lismy@192.168.1.36>
2024-10-09 12:09:57 +0530

    Sample commit

---
+++ FirstMavenProject/src/main/java/samplemavenpackage/Sample.java
@@ -0,0 +1,14 @@
+package samplemavenpackage;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=15;
+		int sum=a+b;
+		System.out.println("Sum is: "+sum);
+
+	}
+
+}
