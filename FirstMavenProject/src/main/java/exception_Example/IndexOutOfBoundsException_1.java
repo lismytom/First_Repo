84fae7c47904f5c9771f230180e18d431d36554f
lismy <lismy@192.168.1.36>
2024-10-18 09:50:32 +0530

    Exceptions

---
+++ FirstMavenProject/src/main/java/exception_Example/IndexOutOfBoundsException_1.java
@@ -0,0 +1,11 @@
+package exception_Example;
+
+public class IndexOutOfBoundsException_1 
+{
+	public static void main(String[] args) 
+	{
+		int arr1[] = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i <= arr1.length; i++)
+            System.out.println(arr1[i]);
+	}
+}
---
+++ FirstMavenProject/src/main/java/exception_Example/NumberFormatException_1.java
@@ -0,0 +1,9 @@
+package exception_Example;
+
+public class NumberFormatException_1 
+{
+	public static void main(String[] args) 
+	{
+		  int a = Integer.parseInt(""); 
+	}
+}
